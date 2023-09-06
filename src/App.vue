520808d3df3a111dd5146b59781d047471f1a3e1
Luis Henrique <luishenriqueribeiro3@gmail.com>
2023-09-06 01:19:53 -0300

    começando um novo projeto e construindo sue primeiro modulo e adicionando um css simples

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      ???
+    </div>
+    <div class="column is-three-quarter">
+
+</div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+
+});
+</script>
+
+<style>
+
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+import { difeneComponent } from 'vue'
+
+export default difeneComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<style scoped>
+/* todo o estilo do componenete */
+header {
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+}
+</style>
