7dbbf14f6783af76c93edab29bef7cf931ad9b4f
Luis Henrique <luishenriqueribeiro3@gmail.com>
2023-09-08 14:29:48 -0300

    criando css para mobile e adicionando um paddind de um rem para o style dos desk

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -17,8 +17,16 @@ export default defineComponent({
 <style scoped>
 /* todo o estilo do componenete */
 header {
+    padding: 1rem;
     background: #0d3b66;
     width: 100%;
     height: 100vh;
+}
+
+@media only screen and (max-width: 768px ) {
+    header {
+        padding-block-start: 2.5rem;
+        height: auto;
+    }
 }
 </style>
