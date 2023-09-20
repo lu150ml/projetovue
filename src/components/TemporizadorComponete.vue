cedec663176afd440a61a309bb3ec3956b28e27e
Luis Henrique <luishenriqueribeiro3@gmail.com>
2023-09-20 03:19:35 -0300

    implementando uma regra para que uma inicialização para evitar  um problema no contador de sobreposição de inicialização

--- src/components/TemporizadorComponete.vue
+++ src/components/TemporizadorComponete.vue
@@ -13,13 +13,13 @@
   <div class="columns">
     <div class="is-flex is-align-items-center is-justify-content-space-between">
       <CronometroTimer :tempoEmSegundos="tempoEmSegundos" />
-      <button class="button" @click="iniciar">
+      <button class="button" @click="iniciar" :disabled="cronometroRodando">
         <span class="icon">
           <i class="fas fa-play"></i>
         </span>
         <span>play</span>
       </button>
-      <button class="button" @click="finalizar">
+      <button class="button" @click="finalizar" :disabled="!cronometroRodando">
         <span class="icon">
           <i class="fas fa-stop"></i>
         </span>
@@ -42,17 +42,20 @@ export default defineComponent({
     return {
       tempoEmSegundos: 0,
       cronometro: 0,
+      cronometroRodando:false,
     };
   },
   methods: {
     iniciar() {
       //começar a  contagem
       // 1 seg = 100ms
+      this.cronometroRodando = true
       this.cronometro = setInterval(() => {
         this.tempoEmSegundos += 1;
       }, 1000);
     },
     finalizar() {
+      this.cronometroRodando = false
       clearInterval(this.cronometro);
     },
   },
